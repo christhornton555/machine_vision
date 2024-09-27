97af042f6ec7b8ba3dc607f4e9f6b791fe2b5724
Chris Thornton <cjthornton78@gmail.com>
2024-09-27 01:13:48 +0100

    A bit more testing

--- test_torch.py
+++ test_torch.py
@@ -1,4 +1,11 @@
+# import torch
+# print(torch.cuda.is_available())  # This should return True if CUDA is available
+# print(torch.cuda.current_device())  # This should return the GPU device ID
+# print(torch.cuda.get_device_name(torch.cuda.current_device()))  # This should return the name of your GPU
+
 import torch
-print(torch.cuda.is_available())  # This should return True if CUDA is available
-print(torch.cuda.current_device())  # This should return the GPU device ID
-print(torch.cuda.get_device_name(torch.cuda.current_device()))  # This should return the name of your GPU
+print(torch.__version__)
+print(torch.version.cuda)
+
+from torchvision import ops
+print(ops.nms.__module__)
