0fe1edb4245b7ce2f623a94fc9dd56a9dc1a2efc
Chris Thornton <cjthornton78@gmail.com>
2024-09-27 23:56:49 +0100

    Fixed a couple of things

--- utils/test_cam_control_opencv.py
+++ utils/test_cam_control_opencv.py
@@ -1,6 +1,6 @@
 import cv2
 
-# Initialize video capture for the Logitech C920 (assuming source 0)
+# Initialize video capture for the camera (assuming source 0)
 cap = cv2.VideoCapture(0)
 
 # Check if the camera is opened
@@ -12,15 +12,15 @@ if not cap.isOpened():
 cap.set(cv2.CAP_PROP_AUTOFOCUS, 0)  # Turn off auto-focus
 
 # Set focus manually (value depends on the camera)
-focus_value = 0  # Adjust this value between 0 (near) and 255 (far)
+focus_value = 255  # Adjust this value between 0 (near) and 255 (far)
 cap.set(cv2.CAP_PROP_FOCUS, focus_value)
 
 # Other capture settings
 cap.set(cv2.CAP_PROP_FPS, 30.0)
 cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter.fourcc('m','j','p','g'))
 cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter.fourcc('M','J','P','G'))
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1365)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 768)
 
 # Main loop to capture frames
 while True:
